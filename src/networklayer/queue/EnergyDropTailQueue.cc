60d6814fbe13d0c3076b6e705c0a5bf7250e586c
Marcin Mincer <marcin.mincer@gmail.com>
2011-07-01 00:44:44 +0200

    EnergyDropTailQ and changed testte_routing example
    With change chanell event, Energy Q set up in .ini

---
+++ src/networklayer/queue/EnergyDropTailQueue.cc
@@ -0,0 +1,80 @@
+//
+// Copyright (C) 2005 Andras Varga
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+
+
+#include <omnetpp.h>
+#include "EnergyDropTailQueue.h"
+
+
+Define_Module(EnergyDropTailQueue);
+
+void EnergyDropTailQueue::initialize()
+{
+    PassiveQueueBase::initialize();
+    queue.setName("l2queue");
+
+    qlenVec.setName("queue length");
+    dropVec.setName("drops");
+
+    outGate = gate("out");
+    energy = 0;
+
+    // configuration
+    frameCapacity = par("frameCapacity");
+}
+
+bool EnergyDropTailQueue::enqueue(cMessage *msg)
+{
+    if (frameCapacity && queue.length() >= frameCapacity)
+    {
+        EV << "Queue full, dropping packet.\n";
+        delete msg;
+        dropVec.record(1);
+        return true;
+    }
+    else
+    {
+        queue.insert(msg);
+        qlenVec.record(queue.length());
+        energy++;
+        return false;
+    }
+}
+
+cMessage *EnergyDropTailQueue::dequeue()
+{
+    if (queue.empty())
+        return NULL;
+
+   cMessage *pk = (cMessage *)queue.pop();
+
+    // statistics
+    qlenVec.record(queue.length());
+    energy++;
+    return pk;
+}
+
+void EnergyDropTailQueue::sendOut(cMessage *msg)
+{
+    send(msg, outGate);
+}
+
+void EnergyDropTailQueue::finish()
+{
+    PassiveQueueBase::finish();
+    recordScalar("energy used by router", energy);
+}
---
+++ src/networklayer/queue/EnergyDropTailQueue.h
@@ -0,0 +1,66 @@
+//
+// Copyright (C) 2005 Andras Varga
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+
+
+#ifndef __INET_ENERGYDROPTAILQUEUE_H
+#define __INET_ENERGYDROPTAILQUEUE_H
+
+#include <omnetpp.h>
+#include "PassiveQueueBase.h"
+
+/**
+ * Drop-front queue. See NED for more info.
+ */
+class INET_API EnergyDropTailQueue : public PassiveQueueBase
+{
+  protected:
+    // configuration
+    int frameCapacity;
+
+    // state
+    cQueue queue;
+    cGate *outGate;
+
+    // statistics
+    cOutVector qlenVec;
+    cOutVector dropVec;
+    long energy;
+
+  protected:
+    virtual void initialize();
+
+    /**
+     * Redefined from PassiveQueueBase.
+     */
+    virtual bool enqueue(cMessage *msg);
+
+    /**
+     * Redefined from PassiveQueueBase.
+     */
+    virtual cMessage *dequeue();
+
+    /**
+     * Redefined from PassiveQueueBase.
+     */
+    virtual void sendOut(cMessage *msg);
+
+    virtual void finish();
+};
+
+#endif
+
+
